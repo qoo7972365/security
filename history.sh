4d616a3ef1bda634e23bc97bcd0ba22d919879d3
qoo7972365 <32336596+qoo7972365@users.noreply.github.com>
2022-10-04 19:02:12 +0800

    Update history.sh

--- history.sh
+++ history.sh
@@ -5,4 +5,4 @@ login_user=$(echo $whoami_txt | awk '{print $1}')
 login_tty=$(echo $whoami_txt | awk '{print $2}')
 > .bash_history
 export PROMPT_COMMAND='{ date "+%Y-%m-%d %T ## `hostname` ## $login_user ## $login_tty ## $login_ip ## USER=$(whoami) PWD=$(pwd) CMD=$(history 1| { read x cmd; echo "$cmd"; })"; } >> /var/log/.histlog'
-if [[ -z `readonly -p | grep PROMPT_COMMAND` ]];then readonly PROMPT_COMMAND;fi% 
+if [[ -z `readonly -p | grep PROMPT_COMMAND` ]];then readonly PROMPT_COMMAND;fi
